e941a0345e233452b8ecd2f1187d41ba8b7fa68f
alaa <alaasharshera123@gmail.com>
2025-04-21 12:27:01 +0200

    complete text styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -7,6 +7,14 @@ abstract class Styles {
   static const TextStyle style18 =
       TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w400);
 
+  static const TextStyle styleBold18 =
+      TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600);
+
+  static TextStyle style20 = TextStyle(
+      color: Colors.black.withOpacity(0.8),
+      fontSize: 20,
+      fontWeight: FontWeight.w400);
+
   static const TextStyle style24 =
       TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600);
 }
